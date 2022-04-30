dc1b16d162b9ab54e238d02cbc40efae07121d09
Federico Di Filitto <federico.difilitto@protonmail.com>
2022-04-30 12:52:06 +0200

    feat(be): created communications api

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.db.models import fields
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Communication(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = fields.CharField(max_length=50, blank=True)
+    content = fields.TextField(blank=True)
+    date_published = fields.DateField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+
--- backend/nyb/urls.py
+++ backend/nyb/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from api.views import cat_view
+from api.views import CommunicationViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('communications', CommunicationViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('cat/', cat_view, name="cat-view")
+    path('api/', include(router.urls))
 ]
