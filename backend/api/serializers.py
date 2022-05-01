e43263348e076216b53cbc712e2e1ee3588ace2c
Federico Di Filitto <federico.difilitto@protonmail.com>
2022-05-01 13:56:26 +0200

    fix(api): added nested user serializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,9 +1,15 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, Serializer
+from rest_framework import serializers
 from api.models import Communication
 from django.contrib.auth.models import User
 
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username',)
 
 class CommunicationSerializer(ModelSerializer):
+    author = UserSerializer()
 
     class Meta:
         model = Communication
