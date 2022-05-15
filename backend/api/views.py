e72b9576ded112ac665e4913324660d395e423ca
Federico Di Filitto <federico.difilitto@protonmail.com>
2022-05-15 16:59:28 +0200

    fix(api): filtering queryset based on user

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from api.models import Communication
@@ -9,4 +11,12 @@ from api.serializers import CommunicationSerializer
 class CommunicationViewSet(ModelViewSet):
     queryset = Communication.objects.all()
     serializer_class = CommunicationSerializer
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticated]
 
+
+    def get_queryset(self):
+        user = self.request.user
+        if not isinstance(user, User):
+            raise RuntimeError("A user must be provided to use this view")
+        return super().get_queryset().filter(author=user)
