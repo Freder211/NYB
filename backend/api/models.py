7a3837977d5f3052ac9896dce69c73160adc197e
Federico Di Filitto <federico.difilitto@protonmail.com>
2022-05-21 23:00:05 +0200

    feat(models): created Crew model

--- backend/api/models.py
+++ backend/api/models.py
@@ -4,26 +4,43 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class ModelOwnership(models.Model):
+
+class Crew(models.Model):
+    users = models.ManyToManyField(User, through="Membership", related_name="crews")
+
+
+class Membership(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    crew = models.ForeignKey(Crew, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ["user", "crew"]
+
+
+class CrewContentModel(models.Model):
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    crew = models.ForeignKey(Crew, on_delete=models.CASCADE, null=True)
+    datetime_created = fields.DateTimeField(auto_now_add=True)
 
     class Meta:
         abstract = True
+        ordering = ["datetime_created"]
 
-class Communication(ModelOwnership):
+
+class Communication(CrewContentModel):
     title = fields.CharField(max_length=50, blank=True)
     content = fields.TextField(blank=True)
     date_published = fields.DateField(auto_now_add=True)
-    
+
     def __str__(self):
         return self.title
 
 
-class Schema(ModelOwnership):
+class Schema(CrewContentModel):
     title = fields.CharField(max_length=50, blank=True)
     description = fields.TextField(blank=True)
     image = models.FileField(blank=True, upload_to="schemas/")
     date_published = fields.DateField(auto_now_add=True)
-    
+
     def __str__(self):
         return self.title
