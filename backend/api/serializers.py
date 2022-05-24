136acbf3a0078e78230cb67d415bec6ee7779198
Federico Di Filitto <federico.difilitto@protonmail.com>
2022-05-25 00:52:08 +0200

    fix(serializers): linted serializers.py

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -3,22 +3,27 @@ from rest_framework import serializers
 from api.models import Communication, Schema
 from django.contrib.auth.models import User
 
+
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username',)
+        fields = (
+            "id",
+            "username",
+        )
+
 
 class OwnershipSerializer(ModelSerializer):
     author = UserSerializer(read_only=True)
 
-class CommunicationSerializer(OwnershipSerializer):
 
+class CommunicationSerializer(OwnershipSerializer):
     class Meta:
         model = Communication
-        fields = '__all__'
+        fields = "__all__"
+
 
 class SchemaSerializer(OwnershipSerializer):
-
     class Meta:
         model = Schema
-        fields = '__all__'
+        fields = "__all__"
